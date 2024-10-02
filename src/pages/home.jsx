bf7597462a67af8272ae1c19ab399b792d28a24c
dzzjio <giojio14@gmail.com>
2024-10-02 21:14:05 +0400

    grind

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,7 +3,7 @@ import heroBg from '../img/vinvar.jpeg';
 const HomePage = () => {
   return (
     <section
-      className="relative min-h-screen bg-cover bg-center flex items-center justify-center"
+      className="relative h-[3000px] bg-cover bg-center flex items-center justify-center"
       style={{ backgroundImage: `url(${heroBg})` }}
     >
       {/* Overlay to darken the background image */}
