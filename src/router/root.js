a685affd40f123dd0ceb3387721ed03ecc55104b
dzzjio <giojio14@gmail.com>
2024-10-03 16:03:40 +0400

    test layout

---
+++ src/router/root.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import Header from '../layout/header'
+import Footer from '../layout/footer'
+
+const Root = () => {
+  return (
+    <div>
+        <Header />
+        <Outlet />
+        <Footer />
+    </div>
+  )
+}
+
+export default Root
--- src/router/router.js
+++ src/router/router.js
@@ -1,15 +1,24 @@
 import { createBrowserRouter } from 'react-router-dom';
+import Root from './root'; // Import Root component
 import Home from '../pages/home';
 import Election from '../pages/election';
+import Error from '../pages/error'; // Optional: Import error page
 
 const Router = createBrowserRouter([
     {
-        path: '',
-        element: <Home />
-    },
-    {
-        path: "/election",
-        element: <Election />
+        path: '/',  // Root layout route
+        element: <Root />,  // Wraps children with the Root component
+        errorElement: <Error />,  // Optional: Handle errors like 404 or other
+        children: [
+            {
+                path: '/',
+                element: <Home />
+            },
+            {
+                path: '/election',
+                element: <Election />
+            }
+        ]
     }
 ]);
 
